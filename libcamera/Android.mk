594e621e1920dbcbe1e65521e5f07209f3187fac
Pavel Kirpichyov <pavel.kirpichyov@gmail.com>
2013-01-08 12:27:11 +0400

    Update camera HAL, get rid of cpu color conversaion
    Change-Id: Iad4e5b008cbed6649dc7730d877bedf3ff192951

--- libcamera/Android.mk
+++ libcamera/Android.mk
@@ -8,6 +8,7 @@ LOCAL_MODULE           := camera.$(TARGET_BOARD_PLATFORM)
 
 LOCAL_MODULE_TAGS      := optional
 
+
 LOCAL_SRC_FILES        := cameraHAL.cpp
 LOCAL_C_INCLUDES       := $(TOP)/frameworks/base/include
 LOCAL_C_INCLUDES       += hardware/qcom/display-legacy/libgralloc
@@ -15,6 +16,6 @@ LOCAL_C_INCLUDES       += hardware/qcom/display-legacy/libgralloc
 LOCAL_SHARED_LIBRARIES := liblog libutils libcutils
 LOCAL_SHARED_LIBRARIES += libui libhardware libcamera_client
 
-LOCAL_LDFLAGS	       += -L$(LOCAL_PATH) -lcamera
+LOCAL_LDFLAGS          += -L$(LOCAL_PATH) -lcamera
 
 include $(BUILD_SHARED_LIBRARY)
