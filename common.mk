6538a61191ff30956e3fd79f5553eb8598b4f3df
erikcas <erikcas1972@gmail.com>
2013-08-12 16:44:12 +0200

    add support for usb host and mount usb disks
    Kanged from KonstaT
    
    (donot forget to adjust overlay/frameworks/base/core/res/res/xml/storage_list.xml in device specific as well

--- common.mk
+++ common.mk
@@ -103,6 +103,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
