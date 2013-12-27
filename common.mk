c007bde4fb856fa86dafd2194e5733045515d88a
Conn O'Griofa <connogriofa@gmail.com>
2013-12-27 17:56:02 +0000

    Update storage configuration & development settings
    * Add debugging properties
    * Allow mass storage mode
    * Update default USB configuration (still using mtp,adb by default)
    
    Change-Id: Ibef617fd0a56c7f1d46092114f65fa867298378e

--- common.mk
+++ common.mk
@@ -194,6 +194,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dexopt-data-only=1 \
     dalvik.vm.dexopt-flags=v=a,o=v,m=y,u=y
 
+## Development settings
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.debuggable=1 \
+    ro.secure=0 \
+    ro.allow.mock.location=0
+
 ## Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.no_hw_vsync=0
@@ -223,8 +229,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.boot.selinux=permissive
 
 ## USB
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=mtp,adb
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
 
 ## WiFi
 PRODUCT_PROPERTY_OVERRIDES += \
