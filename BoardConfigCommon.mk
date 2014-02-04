20afccf02b8125f8cda7b2ded55cb9241a7023a1
Conn O'Griofa <connogriofa@gmail.com>
2014-02-04 04:34:17 +0000

    Split releasetools into regular/tiny (cm10.2)
    * releasetools-extras will no longer exclude basic live wallpapers
    * releasetools-extras-tiny added (no changes as of yet)
    * For now, only galaxy5 will utilise the tiny config.
    
    Change-Id: Ie9b27a1416d4a427cb95e2842c84c0afa4f6a029

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -198,4 +198,9 @@ TARGET_BOOTANIMATION_PRELOAD := true
 TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 
 ## OTA script extras file (build/tools/releasetools)
-TARGET_OTA_EXTRAS_FILE := device/samsung/msm7x27-common/releasetools-extras.txt
+ifneq (,$(filter galaxy5,$(CM_BUILD)))
+	# We can't fit live wallpapers & other features on the /system partition
+	TARGET_OTA_EXTRAS_FILE := device/samsung/msm7x27-common/releasetools-extras-tiny.txt
+else
+	TARGET_OTA_EXTRAS_FILE := device/samsung/msm7x27-common/releasetools-extras.txt
+endif
